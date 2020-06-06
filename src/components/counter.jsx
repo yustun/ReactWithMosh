937899ba7ab9087249c0754a300c8ff9f1c16127
Yasin <yasin.ustun@adesso.com.tr>
2020-06-06 14:41:16 +0300

    Binding Event Handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,42 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/seed/picsum/200/300",
+    imageUrl: "https://picsum.photos/seed/picsum/200/200",
+    tags: [],
+    // tags: ["tag1", "tag2", "tag3"],
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //     console.log("Constructor", this);
+  //   }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no Tags!!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
   };
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}></span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
@@ -20,11 +48,5 @@ class Counter extends Component {
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
 }
-
 export default Counter;
