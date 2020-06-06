1e065570dc44c33a07aca9471258ccdb5fb72a9d
Yasin <yasin.ustun@adesso.com.tr>
2020-06-06 12:49:38 +0300

    Styling Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,17 @@ class Counter extends Component {
     count: 0,
     imageUrl: "https://picsum.photos/seed/picsum/200/300",
   };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
