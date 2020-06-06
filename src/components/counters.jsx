90a3a82aadda1ec981bef0d1f31124fdb5cabb3c
Yasin <yasin.ustun@adesso.com.tr>
2020-06-06 15:11:26 +0300

    Passign Data To Components v2

---
+++ src/components/counters.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+      { id: 4, value: 4 },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((cnt) => (
+          <Counter key={cnt.id} value={cnt.value} selected={true} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
