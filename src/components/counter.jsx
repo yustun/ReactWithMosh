b6624f1641f4e128b587b22db3aaa9e4bfa7a569
Yasin <yasin.ustun@adesso.com.tr>
2020-06-06 12:46:14 +0300

    added counter jsx

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/seed/picsum/200/300",
+  };
+  render() {
+    return (
+      <div>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
