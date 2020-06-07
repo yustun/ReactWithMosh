0a26778206cdf1468df81d31a784737d29ed3a3d
Yasin <yasin.ustun@adesso.com.tr>
2020-06-07 07:31:04 +0300

    add navbar to the top of the page

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,10 +4,10 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 1 },
-      { id: 2, value: 2 },
-      { id: 3, value: 3 },
-      { id: 4, value: 4 },
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
     ],
   };
   render() {
